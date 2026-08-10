33e79e5c2de153b19caf8da8f408e89c7deafca3
LUIS TELLO <luis.tellor@istpargentina.edu.pe>
2026-08-09 19:55:00 -0500

    refactor: adaptar modelo Distrito a PostgreSQL
    Se actualizaron los modelos para coincidir con la nueva estructura y nombres de campos utilizados en PostgreSQL

--- models/distrito.py
+++ models/distrito.py
@@ -1,6 +1,7 @@
 class Distrito:
 
     def __init__(self, id_distrito=0, nombre=""):
+
         self.__id_distrito = id_distrito
         self.__nombre = nombre
 
@@ -21,17 +22,20 @@ class Distrito:
         self.__nombre = valor
 
     def __str__(self):
+
         return f"{self.__id_distrito} - {self.__nombre}"
-    
+
     def to_dict(self):
+
         return {
-            "ID_Distrito": self.__id_distrito,
-            "Nombre": self.__nombre,
+            "id_distrito": self.__id_distrito,
+            "nombre": self.__nombre
         }
 
     @classmethod
     def from_dict(cls, datos):
+
         return cls(
-            datos["ID_Distrito"],
-            datos["Nombre"]
+            datos.get("id_distrito", 0),
+            datos.get("nombre", "")
         )
