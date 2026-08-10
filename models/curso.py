e4345e6b8c614663acecf14ef2bb43e4fd2db2bc
raquel-castro <raquel.castrop@istpargentina.edu.pe>
2026-08-09 20:58:21 -0500

    refactor: adaptar modelo Curso a PostgreSQL
    Se actualizo el modelo para coincidir con la nueva estructura y nombres de campos utilizados en PostgreSQL

--- models/curso.py
+++ models/curso.py
@@ -1,8 +1,15 @@
 class Curso:
 
-    def __init__(self, id_curso=0, nombre="", descripcion="",
-                 creditos=0, ciclo="", horas_semanales=0,
-                 id_docente=0):
+    def __init__(
+        self,
+        id_curso=0,
+        nombre="",
+        descripcion="",
+        creditos=0,
+        ciclo="",
+        horas_semanales=0,
+        id_docente=0
+    ):
 
         self.__id_curso = id_curso
         self.__nombre = nombre
@@ -69,28 +76,38 @@ class Curso:
         self.__id_docente = valor
 
     def __str__(self):
-        return f"{self.__id_curso} - {self.__nombre} - {self.__descripcion} - {self.__creditos} - {self.__ciclo} - {self.__horas_semanales} - {self.__id_docente}"
-    
-    def to_dict(self):   
+
+        return (
+            f"{self.__id_curso} - "
+            f"{self.__nombre} - "
+            f"{self.__descripcion} - "
+            f"{self.__creditos} - "
+            f"{self.__ciclo} - "
+            f"{self.__horas_semanales} - "
+            f"{self.__id_docente}"
+        )
+
+    def to_dict(self):
+
         return {
-            "ID_Curso"        : self.__id_curso,
-            "Nombre"          : self.__nombre,
-            "Descripcion"     : self.__descripcion,
-            "Creditos"        : self.__creditos,
-            "Ciclo"           : self.__ciclo,
-            "Horas Semanales" : self.__horas_semanales,     
-            "ID_Docente"      : self.__id_docente,
-        };
-    
+            "id_curso": self.__id_curso,
+            "nombre": self.__nombre,
+            "descripcion": self.__descripcion,
+            "creditos": self.__creditos,
+            "ciclo": self.__ciclo,
+            "horas_semanales": self.__horas_semanales,
+            "id_docente": self.__id_docente
+        }
+
     @classmethod
     def from_dict(cls, datos):
 
         return cls(
-            datos["ID_Curso"],
-            datos["Nombre"],
-            datos["Descripcion"],
-            datos["Creditos"],
-            datos["Ciclo"],
-            datos["Horas Semanales"],
-            datos["ID_Docente"]
+            datos.get("id_curso", 0),
+            datos.get("nombre", ""),
+            datos.get("descripcion", ""),
+            datos.get("creditos", 0),
+            datos.get("ciclo", ""),
+            datos.get("horas_semanales", 0),
+            datos.get("id_docente", 0)
         )
