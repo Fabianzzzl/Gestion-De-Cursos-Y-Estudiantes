88e91978528b31112493ef4c7424173758bcd9cc
LUIS TELLO <luis.tellor@istpargentina.edu.pe>
2026-08-10 00:11:44 -0500

    Update persona_schema.py

--- schemas/persona_schema.py
+++ schemas/persona_schema.py
@@ -1,11 +1,8 @@
 import re
-
 from pydantic import BaseModel, field_validator
 from typing import Optional
 
-
 class PersonaCrear(BaseModel):
-
     dni: str
     nombres: str
     apellidos: str
@@ -13,22 +10,16 @@ class PersonaCrear(BaseModel):
     correo: Optional[str] = None
     direccion: Optional[str] = None
 
-
     @field_validator("dni")
     @classmethod
     def validar_dni(cls, valor):
-
         if not re.fullmatch(r"\d{8}", valor):
-
             raise ValueError(
                 "El DNI debe tener exactamente 8 dígitos numéricos"
             )
-
         return valor
 
-
 class PersonaActualizar(BaseModel):
-
     dni: Optional[str] = None
     nombres: Optional[str] = None
     apellidos: Optional[str] = None
@@ -36,25 +27,19 @@ class PersonaActualizar(BaseModel):
     correo: Optional[str] = None
     direccion: Optional[str] = None
 
-
     @field_validator("dni")
     @classmethod
     def validar_dni(cls, valor):
-
         if valor is not None and not re.fullmatch(
             r"\d{8}",
             valor
         ):
-
             raise ValueError(
                 "El DNI debe tener exactamente 8 dígitos numéricos"
             )
-
         return valor
 
-
 class PersonaRespuesta(BaseModel):
-
     id_persona: int
     dni: str
     nombres: str
